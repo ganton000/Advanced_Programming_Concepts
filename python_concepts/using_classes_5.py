30103eaed5880bf27b0f7578fa631bb7ce26d7c0
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-07 16:00:09 -0500

    script_5 showcases worker classes and threading with polymorphic interfaces

--- python_concepts/using_classes_5.py
+++ python_concepts/using_classes_5.py
@@ -1,3 +1,8 @@
+import os
+import random
+from threading import Thread
+from tempfile import TemporaryDirectory
+
 
 class InputData(object):
 
@@ -39,5 +44,48 @@ class LineCountWorker(Worker):
 
 		self.result += other.result
 
+def generate_inputs(data_dir):
+	## creates path to data file based on every file in directory
+	for name in os.listdir(data_dir):
+		yield PathInputData(os.path.join(data_dir, name))
+
+def create_workers(input_list):
+	workers = []
+
+	for input_data in input_list:
+		workers.append(LineCountWorker(input_data))
+
+	return workers
+
+def execute(workers):
+	threads = [Thread(target=w.map) for w in workers]
+
+	for thread in threads: thread.start()
+	for thread in threads: thread.join() # waits for threads to finish
+
+	first, rest_of_workers = workers[0], workers[1:]
+
+	for worker in rest_of_workers:
+		# final result sits in first worker's result
+		first.reduce(worker)
+
+	return first.result
+
+def mapreduce(data_dir):
+	inputs = generate_inputs(data_dir)
+	workers = create_workers(inputs)
+	return execute(workers)
+
+def write_test_files(tmpdir):
+	''' creates 100 test files with random data '''
+	for i in range(100):
+		with open(os.path.join(tmpdir, str(i)), "w") as handle:
+			handle.write("\n" * random.randint(0,100))
+
+
 if __name__ == "__main__":
-	pass
+
+	with TemporaryDirectory() as tmpdir:
+		write_test_files(tmpdir)
+		result = mapreduce(tmpdir)
+		print(result)
