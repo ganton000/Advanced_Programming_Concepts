598244db6f8e3c5d5492c53c400873c4dd7bfa98
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-09 16:57:47 -0500

    Added lock to class

--- python_concepts/using_threads_and_locking.py
+++ python_concepts/using_threads_and_locking.py
@@ -5,11 +5,21 @@ class Counter(object):
 
 	def __init__(self):
 		self.count = 0
+		self.lock = threading.Lock()
 
 	def increment(self, offset):
-		self.count += offset
+		with self.lock:
+			self.count += offset
+		'''
+		above line of code is equivalent to the bottom 3 lines
+		So with multi-threading without locks, execution can be suspended in between any of these 3 lines, altering expected results.
+		'''
+		#value = getattr(self, "count")
+		#result = value + offset
+		#setattr(self, "count", result)
 
 def worker(sensor_index, how_many, counter):
+	barrier.wait()
 	for _ in range(how_many):
 		# Do the sensor readings
 		counter.increment(1)
@@ -21,7 +31,7 @@ if __name__ == "__main__":
 	barrier = threading.Barrier(worker_count)
 
 	threads = []
-	how_many = 10000
+	how_many = 1000000
 	counter = Counter()
 	for i in range(worker_count):
 		args = (i, how_many, counter)
