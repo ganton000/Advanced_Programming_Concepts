4d82a043c194ca39d961223f774849e38bb0478f
ganton000 <SendittoGeorgeAnton@gmail.com>
2023-02-13 23:27:51 -0500

    added visual

--- python_concepts/particle_simulator/particle.py
+++ python_concepts/particle_simulator/particle.py
@@ -15,7 +15,7 @@ class ParticleSimulator:
 	def __init__(self, particles: List[Particle]) -> None:
 		self.particles = particles
 
-	def evolve(self, dt):
+	def evolve(self, dt: int) -> None:
 		timestep = 0.0001
 		nsteps = int(dt/timestep)
 
@@ -31,3 +31,51 @@ class ParticleSimulator:
 
 				p.x += d_x
 				p.y += d_y
+
+def visualize(simulator: ParticleSimulator) -> None:
+	X = [p.x for p in simulator.particles]
+	Y = [p.y for p in simulator.particles]
+
+	fig = plt.figure()
+	ax = plt.subplot(111, aspect="equal")
+	line, = ax.plot(X,Y, "ro")
+
+	#Axis lims
+	plt.xlim(-1, 1)
+	plt.ylim(-1, 1)
+
+	def init():
+		line.set_data([], [])
+		return line,
+
+	def animate(i):
+		simulator.evolve(0.01)
+		X = [p.x for p in simulator.particles]
+		Y = [p.y for p in simulator.particles]
+		line.set_data(X,Y)
+		return line,
+
+	# call animate every 10 ms
+	anim = animation.FuncAnimation(
+		fig,
+		animate,
+		init_func=init,
+		blit=True,
+		interval=10
+	)
+	plt.show()
+
+def test_visualize():
+
+	particles = [
+		Particle(0.3, 0.5, 1),
+		Particle(0.0, -0.5, -1),
+		Particle(-0.1, -0.4, 3)
+	]
+
+	simulator = ParticleSimulator(particles)
+	visualize(simulator)
+
+if __name__ == "__main__":
+
+	test_visualize()
